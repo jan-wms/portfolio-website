3a4e09234f78f799029bee00d72efe49138b427d
jan2210 <jan.wermeckes@gmx.de>
2022-11-02 12:51:16 +0100

    2022-11-02

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,7 +6,27 @@ const Footer = () => {
     <footer className={styles.footer}>
       <section>
         <h2>Kontakt</h2>
-        <p>Kontaktformular</p>
+        <form method="post" netlify-honeypot="bot-field" data-netlify="true" name="contact">
+          <input type="hidden" name="bot-field" />
+          <input type="hidden" name="form-name" value="contact" />
+          <label>
+            Name
+            <input type="text" name="name" id="name" />
+          </label>
+          <label>
+            Email
+            <input type="email" name="email" id="email" />
+          </label>
+          <label>
+            Subject
+            <input type="text" name="subject" id="subject" />
+          </label>
+          <label>
+            Message
+            <textarea name="message" id="message" rows="5" />
+          </label>
+          <button type="submit">Abschicken</button>
+        </form>
       </section>
       <p>© 2022 Jan Wermeckes</p>
     </footer>
