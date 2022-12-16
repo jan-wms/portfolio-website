4d233786a49c74a2c4ab8fb077cb02ff72273bb3
jan2210 <jan.wermeckes@gmx.de>
2022-12-16 17:13:49 +0100

    2022-12-16

--- src/components/header/headerGraphic.jsx
+++ src/components/header/headerGraphic.jsx
@@ -30,31 +30,96 @@ import { GiMountainClimbing } from 'react-icons/gi'
 const HeaderGraphic = () => {
   return (
     <section className={styles.icons}>
+      {/**
+       * Left arrow
+       */}
       <FaComments />
       <FaGlobeAmericas />
       <FaFolder />
-      <BsClock />
       <AiOutlineWifi />
       <HiOutlineCode />
       <GiMountainClimbing />
-      <IoMdPerson />
       <FaBook />
       <FaRunning />
-      <FaPen />
       <BsFillCameraVideoFill />
       <AiOutlineZoomIn />
       <IoPhonePortraitOutline />
       <IoIosSunny />
       <IoIosMail />
-      <FaSquareRootAlt />
       <IoMdMusicalNote />
-      <FaVolumeUp style={{ left: '3%', top: '56%', height: '5%'}} color='#b49bc9'/>
+      <BsClock/>
+
+      {/*<FaPen style={{ left: '6%', top: '59%', height: '4%'}} color='#cfc695'/>*/}
+      <FaSquareRootAlt style={{ left: '5.5%', top: '58%', height: '6%'}} color='#92bff9'/>
+      <FaVolumeUp style={{ left: '2.8%', top: '56%', height: '5%'}} color='#b49bc9'/>
       <FaMountain style={{ left: '1%', top: '49%', height: '8%'}} color='#bfbfbf'/>
       <FaHeart style={{ left: '8%', top: '42%', height: '6%'}} color='#ed4949'/>
       <MdThumbUp style={{ left: '13%', top: '44%', height: '6%'}} color='#cc8bdd'/>
       <FaArrowRight style={{ left: '13%', top: '38%', height: '6%'}} color='#5b7f5a'/>
       <RiSettings4Fill style={{ left: '7%', top: '48%', height: '8%'}} color='#2aadbf'/>
       <FaPlane style={{ left: '5%', top: '45%', height: '6%'}} color='#ba970b' />
+      <IoMdPerson style={{ left: '16.5%', top: '37.5%', height: '8%'}} color='#505ceb'/>
+
+      {/**
+       * Slash
+       */}
+      <FaGlobeAmericas />
+      <FaFolder />
+      <AiOutlineWifi />
+      <HiOutlineCode />
+      <GiMountainClimbing />
+      <FaRunning />
+      <BsFillCameraVideoFill />
+      <AiOutlineZoomIn />
+      <IoPhonePortraitOutline />
+      <IoIosSunny />
+      <IoIosMail />
+      <IoMdMusicalNote />
+      <BsClock/>
+
+      <FaPen style={{ left: '50%', top: '59%', height: '4%'}} color='#cfc695'/>
+      <FaSquareRootAlt style={{ left: '50%', top: '58%', height: '6%'}} color='#92bff9'/>
+      <FaVolumeUp style={{ left: '50%', top: '56%', height: '5%'}} color='#b49bc9'/>
+      <FaMountain style={{ left: '50%', top: '49%', height: '8%'}} color='#bfbfbf'/>
+      <MdThumbUp style={{ left: '50%', top: '44%', height: '6%'}} color='#cc8bdd'/>
+      <FaArrowRight style={{ left: '50%', top: '38%', height: '6%'}} color='#5b7f5a'/>
+      <RiSettings4Fill style={{ left: '50%', top: '48%', height: '8%'}} color='#2aadbf'/>
+      <FaPlane style={{ left: '50%', top: '45%', height: '6%'}} color='#ba970b' />
+      <IoMdPerson style={{ left: '50%', top: '37.5%', height: '8%'}} color='#505ceb'/>
+
+
+      <FaBook style={{ left: '70%', top: '7%', height: '6%'}} color='#2aadbf'/>
+      <FaComments style={{ left: '35%', top: '85%', height: '8%'}} color='#a3b3c1'/>
+
+      {/**
+       * Right arrow
+       */}
+      <FaComments />
+      <FaGlobeAmericas />
+      <FaFolder />
+      <AiOutlineWifi />
+      <HiOutlineCode />
+      <GiMountainClimbing />
+      <FaBook />
+      <FaRunning />
+      <BsFillCameraVideoFill />
+      <AiOutlineZoomIn />
+      <IoPhonePortraitOutline />
+      <IoIosSunny />
+      <IoIosMail />
+      <IoMdMusicalNote />
+      <BsClock/>
+
+      <FaPen style={{ left: '90%', top: '59%', height: '4%'}} color='#cfc695'/>
+      <FaSquareRootAlt style={{ left: '90%', top: '58%', height: '6%'}} color='#92bff9'/>
+      <FaVolumeUp style={{ left: '90%', top: '56%', height: '5%'}} color='#b49bc9'/>
+      <FaMountain style={{ left: '90%', top: '49%', height: '8%'}} color='#bfbfbf'/>
+      <FaHeart style={{ left: '90%', top: '42%', height: '6%'}} color='#ed4949'/>
+      <MdThumbUp style={{ left: '90%', top: '44%', height: '6%'}} color='#cc8bdd'/>
+      <FaArrowRight style={{ left: '90%', top: '38%', height: '6%'}} color='#5b7f5a'/>
+      <RiSettings4Fill style={{ left: '90%', top: '48%', height: '8%'}} color='#2aadbf'/>
+      <FaPlane style={{ left: '90%', top: '45%', height: '6%'}} color='#ba970b' />
+      <IoMdPerson style={{ left: '90%', top: '37.5%', height: '8%'}} color='#505ceb'/>
     </section>
   )
 }
