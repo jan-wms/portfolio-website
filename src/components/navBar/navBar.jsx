e798285b0752bbd355eb879d69608eb6fe0403b8
jan2210 <jan.wermeckes@gmx.de>
2023-03-08 17:08:41 +0100

    2023-03-08

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -4,24 +4,28 @@ import { AnchorLink } from "gatsby-plugin-anchor-links";
 import { HiMenu } from "@react-icons/all-files/hi/HiMenu"
 import { HiX } from "@react-icons/all-files/hi/HiX"
 
-
 const NavBar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const toggleIsMenuOpen = () => {
-        setIsMenuOpen(!isMenuOpen);
+    const openMenu = () => {
+        setIsMenuOpen(true);
+        document.body.style.overflow = "hidden"
+    };
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+        document.body.style.overflow = "auto"
     };
 
     return (
         <nav className={styles.wrapper}>
             {
-                isMenuOpen ? (<HiX className={styles.icon} onClick={toggleIsMenuOpen} />) : (<HiMenu className={styles.icon} onClick={toggleIsMenuOpen} />)
+                isMenuOpen ? (<HiX className={styles.icon} onClick={closeMenu} />) : (<HiMenu className={styles.icon} onClick={openMenu} />)
             }
 
             <ul className={`${styles.navBarList} ${isMenuOpen ? styles.navBarListActive : ''}`}>
-                <li className={styles.normalLi}><AnchorLink to="#about" title="Über mich"><span>Über mich</span></AnchorLink></li>
-                <li className={styles.normalLi}><AnchorLink to="#projects" title="Projekte"><span>Projekte</span></AnchorLink></li>
-                <li className={styles.normalLi}><AnchorLink to="#github" title="Github"><span>Github</span></AnchorLink></li>
-                <li className={styles.buttonLi}><AnchorLink to="#footer" title="Kontakt"><span>Kontakt</span></AnchorLink></li>
+                <li className={styles.normalLi} onClick={closeMenu}><AnchorLink to="#about" title="Über mich"><span>Über mich</span></AnchorLink></li>
+                <li className={styles.normalLi} onClick={closeMenu}><AnchorLink to="#projects" title="Projekte"><span>Projekte</span></AnchorLink></li>
+                <li className={styles.normalLi} onClick={closeMenu}><AnchorLink to="#github" title="Github"><span>Github</span></AnchorLink></li>
+                <li className={styles.buttonLi} onClick={closeMenu}><AnchorLink to="#footer" title="Kontakt"><span>Kontakt</span></AnchorLink></li>
             </ul>
 
         </nav>
