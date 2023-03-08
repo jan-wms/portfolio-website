52bf7324071f324249f7c11c7136fd7ab287ae90
jan2210 <jan.wermeckes@gmx.de>
2023-03-08 16:02:50 +0100

    2023-03-08

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as styles from './navBar.module.css'
 import { AnchorLink } from "gatsby-plugin-anchor-links";
+import { HiMenu } from "@react-icons/all-files/hi/HiMenu"
+import { HiX } from "@react-icons/all-files/hi/HiX"
+
 
 const NavBar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const toggleIsMenuOpen = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
-        <nav className={styles.navBar}>
-            <ul>
+        <nav className={styles.wrapper}>
+            {
+                isMenuOpen ? (<HiX className={styles.icon} onClick={toggleIsMenuOpen} />) : (<HiMenu className={styles.icon} onClick={toggleIsMenuOpen} />)
+            }
+
+            <ul className={`${styles.navBarList} ${isMenuOpen ? styles.navBarListActive : ''}`}>
                 <li className={styles.normalLi}><AnchorLink to="#about" title="Über mich"><span>Über mich</span></AnchorLink></li>
                 <li className={styles.normalLi}><AnchorLink to="#projects" title="Projekte"><span>Projekte</span></AnchorLink></li>
                 <li className={styles.normalLi}><AnchorLink to="#github" title="Github"><span>Github</span></AnchorLink></li>
                 <li className={styles.buttonLi}><AnchorLink to="#footer" title="Kontakt"><span>Kontakt</span></AnchorLink></li>
             </ul>
+
         </nav>
     )
 }
