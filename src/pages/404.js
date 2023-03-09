469df6554f76889fffb5aa481eafcc2f52105957
jan2210 <jan.wermeckes@gmx.de>
2023-03-09 11:57:09 +0100

    2023-03-08

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,19 +1,23 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import './404.css'
+import * as styles from './404.module.css'
 
 
 const NotFoundPage = () => {
   return (
     <main>
-      <h1>404</h1>
-      <h2>Seite nicht gefunden</h2>
-      
-      <Link to="/">Zur Startseite</Link>
+      <div className={styles.wrapper}>
+        <section className={styles.content}>
+          <h1>404</h1>
+          <h2>Seite nicht gefunden</h2>
+
+          <Link to="/" className={styles.link}>Zur Startseite</Link>
+        </section>
+      </div>
     </main>
   )
 }
 
 export default NotFoundPage
 
-export const Head = () => <title>404 | Seite nicht gefunden</title>
+export const Head = () => <title>404 • Seite nicht gefunden • Jan Wermeckes</title>
