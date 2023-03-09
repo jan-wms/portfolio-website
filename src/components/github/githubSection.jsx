c7e51739ee0b2f73f23f912d3375b2657c0df624
jan2210 <jan.wermeckes@gmx.de>
2023-03-09 13:45:26 +0100

    2023-03-08

--- src/components/github/githubSection.jsx
+++ src/components/github/githubSection.jsx
@@ -1,8 +1,13 @@
 import { useStaticQuery } from 'gatsby';
 import { graphql } from 'gatsby'
-import React from 'react'
 import GithubCard from './githubCard/githubCard';
 import * as styles from './githubSection.module.css'
+import React, { useState, useEffect, useRef } from 'react'
+import {
+    motion,
+    useScroll,
+    useTransform,
+} from "framer-motion";
 
 const GithubSection = () => {
     const githubReposData = useStaticQuery(graphql`
@@ -18,14 +23,41 @@ const GithubSection = () => {
       }
   `);
 
+    //title heigth
+    const [titleHeight, setHeight] = useState(0)
+    const titleRef = useRef(null)
+    useEffect(() => {
+        setHeight(titleRef.current.clientHeight)
+    })
+
+    //window height
+    const [windowHeight, detectWH] = useState(typeof window !== "undefined" ? window.innerHeight : 500);
+    useEffect(() => {
+        const detectSize = () => {
+            detectWH(typeof window !== "undefined" ? window.innerHeight : 500);
+        }
+        window.addEventListener('resize', detectSize)
+        return () => {
+            window.removeEventListener('resize', detectSize)
+        }
+    }, [])
+
+    const cardRef = useRef(null);
+    const { scrollYProgress, scrollY } = useScroll({
+        target: cardRef,
+        offset: [`-${(windowHeight - titleHeight) / 2}px`, `${(windowHeight - titleHeight) / 2}px`]
+        //offset: ["start start", "end end"]
+    });
+    const yOffset = useTransform(scrollYProgress, [0, 1], [0, windowHeight - titleHeight]);
+
     return (
         <section className={styles.githubSection} id='github'>
-            <div className={styles.title}>
+            <motion.div ref={titleRef} style={{ translateY: yOffset }} className={styles.title}>
                 <a href='https://www.github.com/jan2210/' target="_blank" rel="noreferrer"></a>
                 <h2>Git<br />Hub</h2>
-            </div>
+            </motion.div>
 
-            <div className={styles.cards}>
+            <div className={styles.cards} ref={cardRef}>
                 {
                     githubReposData.allGithubReposJson.edges.map((item, index) => {
                         return (
