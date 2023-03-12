a1548e747119eae40ccf57107c3298627ed9d9f4
jan2210 <jan.wermeckes@gmx.de>
2023-03-12 13:05:48 +0100

    2023-03-12

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import * as React from "react"
 import "./global.css"
-import NavBar from '../components/navBar/NavBar'
-import Header from '../components/header/Header'
-import About from '../components/about/About'
-import Projects from '../components/projects/Projects'
-import GithubSection from '../components/github/GithubSection'
-import Footer from '../components/footer/Footer'
-import Seo from '../components/seo/Seo'
+import NavBar from '../components/navBar/NavBar.jsx'
+import Header from '../components/header/Header.jsx'
+import About from '../components/about/About.jsx'
+import Projects from '../components/projects/Projects.jsx'
+import GithubSection from '../components/github/GithubSection.jsx'
+import Footer from '../components/footer/Footer.jsx'
+import Seo from '../components/seo/Seo.jsx'
 
 const navBarLinks = [
   { url: "/#about", title: "Über mich", isButtonStyle: false },
